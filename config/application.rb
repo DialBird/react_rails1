c3aa51711572c1db76105b222ced61b1ba30e091
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-12-26 16:46:04 +0900

    [master] config設定

--- config/application.rb
+++ config/application.rb
@@ -24,12 +24,15 @@ module ReactRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.time_zone = "Tokyo"
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.decorator false
+      g.test_framework nil
+    end
   end
 end
