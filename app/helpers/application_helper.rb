b77c18f0fdcb9b8656e70c576fd3f4e6fd676cb0
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-12-27 12:08:23 +0900

    [master] setup flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def display_flash
+    flash.each do |type, msg|
+      type = 'success' if type == 'notice'
+      type = 'danger' if %w[alert error].include? type
+      concat tag.div msg, class: "alert alert-#{type}"
+    end
+    nil
+  end
 end
