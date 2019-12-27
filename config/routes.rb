74d01b267f35059b2143cc157b8b8a81c1787172
Keisuke Taniguchi <rememberthatk@icloud.com>
2019-12-27 09:10:12 +0900

    [master] add DashboardController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#show'
+  get '/dashboard' => 'dashboard#show', as: :user_root
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
