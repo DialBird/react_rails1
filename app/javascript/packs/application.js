ff9d59b947f4987af0c2ce264ee174784badb9d7
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-01-04 18:21:06 +0900

    [master] app rails generate react:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,7 @@ import '../src/stylesheets/application.scss';
 
 require('@rails/ujs').start();
 require('@rails/activestorage').start();
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
