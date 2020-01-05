4223c044e2d06444a54d10fcdfa45d7028c75c6d
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-01-05 14:42:49 +0900

    [master] add hello world

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,13 @@ import 'bootstrap';
 import 'parsleyjs';
 import 'popper.js';
 
-import '../src/main'
+import '../src/main';
 
 import '../src/stylesheets/application.scss';
 
 require('@rails/ujs').start();
 require('@rails/activestorage').start();
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
+let componentRequireContext = require.context('components', true);
+let ReactRailsUJS = require('react_ujs');
 ReactRailsUJS.useContext(componentRequireContext);
