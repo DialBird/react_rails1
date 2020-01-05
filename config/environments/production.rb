b5240d37cf6e88ccbb960dc195a7710d1e9e51ac
Keisuke Taniguchi <rememberthatk@icloud.com>
2020-01-05 19:12:20 +0900

    [master] enable to mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { protocol: 'https', host: ENV['DOMAIN_NAME'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:mail_trap, :user_name),
+    password: Rails.application.credentials.dig(:mail_trap, :password),
+    address: 'smtp.mailtrap.io',
+    domain: 'smtp.mailtrap.io',
+    port: '2525',
+    authentication: :cram_md5
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
